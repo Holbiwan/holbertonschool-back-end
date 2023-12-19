2445fe2f1ca50b5c6bc800c85751f3ce12bdaab6
Holbiwan <sabrinapapeau@gmail.com>
2023-12-19 10:11:54 +0100

    Re-buit code

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
-"""Uses a REST API for a given employee ID, returns
-information  about TODO list progress and exports in CSV"""
-
+"""import"""
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"UsageError: python3 {__file__} employee_id(int)")
+    if len(sys.argv) < 2:
+        print(f"missing employee id as argument")
         sys.exit(1)
 
-    API_URL = "https://jsonplaceholder.typicode.com"
+    URL = "https://jsonplaceholder.typicode.com"
     EMPLOYEE_ID = sys.argv[1]
 
-    response = requests.get(
-        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
-        params={"_expand": "user"}
-    )
+    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos",
+                                  params={"_expand": "user"})
+    data = EMPLOYEE_TODOS.json()
 
-    if response.status_code != 200:
-        print(f"RequestError: {response.status_code}")
-        sys.exit(1)
+    EMPLOYEE_NAME = data[0]["user"]["username"]
+    fileName = f"{EMPLOYEE_ID}.csv"
 
-    data = response.json()
-
-    if not data:
-        print("No TODO data found for the given employee ID.")
-        sys.exit(1)
-
-    username = data[0]["user"]["username"]
-
-    with open(f"{EMPLOYEE_ID}.csv", "w", newline="") as file:
+    with open(fileName, "w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
         for task in data:
             writer.writerow(
-                [EMPLOYEE_ID, username, str(task["completed"]), task["title"]]
+                [EMPLOYEE_ID, EMPLOYEE_NAME, str(task["completed"]),
+                 task["title"]]
             )
-
-    print(f"CSV file '{EMPLOYEE_ID}.csv' has been successfully created.")
+        
